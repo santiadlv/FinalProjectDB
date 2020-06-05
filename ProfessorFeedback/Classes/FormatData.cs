9a7c00ad6de4a83d07eff2b3df9a0b7a1cfe75bf
santiadlv <49079265+santiadlv@users.noreply.github.com>
2020-06-05 14:21:40 -0600

    Added data formatting class and functions

---
+++ ProfessorFeedback/Classes/FormatData.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfessorFeedback.Classes
+{
+    static class FormatData
+    {
+        public static float TryFloatConvert(string input)
+        {
+            float result;
+            try
+            {
+                result = Convert.ToSingle(input);
+            }
+            catch (FormatException)
+            {
+                Console.Write("Data type invalid. Please enter a valid input: ");
+                result = TryFloatConvert(Console.ReadLine());
+            }
+            return result;
+        }
+
+        public static int TryIntConvert(string input)
+        {
+            int result;
+            try
+            {
+                result = Convert.ToInt32(input);
+            }
+            catch (FormatException)
+            {
+                Console.Write("Data type invalid. Please enter a valid input: ");
+                result = TryIntConvert(Console.ReadLine());
+            }
+            return result;
+        }
+
+        public static DateTime TryDateTimeConvert(string input)
+        {
+            DateTime result;
+            try
+            {
+                result = Convert.ToDateTime(input);
+            }
+            catch (FormatException)
+            {
+                Console.Write("Data type invalid. Please enter a valid input: ");
+                result = TryDateTimeConvert(Console.ReadLine());
+            }
+            return result;
+        }
+    }
+}
